795f5f51d4521c2757f57669843f1e35e31a8cce
YJ Soon <something@yjsoon.com>
2023-04-10 17:01:00 +0800

    Initial Commit

---
+++ github demo upload/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  github demo upload
+//
+//  Created by YJ Soon on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github demo upload/github_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demo_uploadApp.swift
+//  github demo upload
+//
+//  Created by YJ Soon on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct github_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
