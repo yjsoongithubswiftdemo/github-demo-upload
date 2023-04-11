731c867c566e63bebdefff09161936e5fb654fdd
YJ Soon <something@yjsoon.com>
2023-04-11 17:39:27 +0800

    changed to potato

--- github demo upload/ContentView.swift
+++ github demo upload/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, potato!")
             Text("This is a bit better")
             Text("I just like more text")
         }
