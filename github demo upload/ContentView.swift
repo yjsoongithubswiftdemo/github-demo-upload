1713d133a13cd6c7ca89b5d412ee11dd952bc4ec
YJ Soon <yjsoon@tinkertanker.com>
2023-04-11 17:38:36 +0800

    changed to pineapple

--- github demo upload/ContentView.swift
+++ github demo upload/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, pineapple!")
             Text("This is a bit better")
             Text("I just like more text")
         }
